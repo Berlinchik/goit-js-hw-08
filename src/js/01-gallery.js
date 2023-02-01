e4a50b48c60dd88f4a8f5706260c50a85647e443
Berlinchik <davidarts11@gmail.com>
2023-02-02 00:07:59 +0200

    hw-8 (task: 1, 2, 3)

---
+++ src/js/01-gallery.js
@@ -0,0 +1,31 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+import SimpleLightbox from 'simpleLightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galaryList = document.querySelector('.gallery');
+function createGalaryMarkup(array) {
+  return array
+    .map(
+      ({ original, preview, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join('');
+}
+galaryList.insertAdjacentHTML('beforeend', createGalaryMarkup(galleryItems));
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
---
+++ src/js/03-feedback.js
@@ -0,0 +1,29 @@
+//імпортую throttle
+import throttle from 'lodash.throttle';
+// отримую доступ до форми і до інпутів
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input');
+const messagelInput = form.querySelector('textarea');
+//якщо в localStorage є якісь збережені данні - підставляю їх в інпути
+const currentData = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (currentData) {
+  emailInput.value = currentData.email;
+  messagelInput.value = currentData.message;
+}
+//вішаю слухачі на форму
+form.addEventListener('input', throttle(pushToServer, 1000));
+form.addEventListener('submit', onSubmitForm);
+//створюю пустий обʼєкт, щоб потім в нього зберегти данні з інпутів
+const obj = {};
+//функція для додавання данних на локальне сховище
+function pushToServer({ target }) {
+  obj[target.name] = target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(obj));
+}
+//при сабміті виводимо дані(взяті з локального сховища) у вигляді обʼєкту в консоль, стираємо інпути та чистимо локальне сховище
+function onSubmitForm(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.target.reset();
+  localStorage.removeItem('feedback-form-state');
+}
