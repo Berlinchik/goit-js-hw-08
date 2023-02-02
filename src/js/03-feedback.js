da74e0ad000696246e531d3e3bc657a31ee11e8f
Berlinchik <davidarts11@gmail.com>
2023-02-02 22:26:16 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,23 @@
-//імпортую throttle
 import throttle from 'lodash.throttle';
-// отримую доступ до форми і до інпутів
+
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input');
 const messagelInput = form.querySelector('textarea');
-//якщо в localStorage є якісь збережені данні - підставляю їх в інпути, якщо данних немає створюємо пустий обʼєкт.
+
 const currentData =
   JSON.parse(localStorage.getItem('feedback-form-state')) || {};
 emailInput.value = currentData.email || '';
 messagelInput.value = currentData.message || '';
-//вішаю слухачі на форму
+
 form.addEventListener('input', throttle(pushToServer, 1000));
 form.addEventListener('submit', onSubmitForm);
-//функція для додавання данних на локальне сховище
+
+const obj = { ...currentData };
 function pushToServer({ target }) {
-  currentData[target.name] = target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(currentData));
+  obj[target.name] = target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(obj));
 }
-//при сабміті виводимо дані(взяті з локального сховища) у вигляді обʼєкту в консоль, стираємо інпути та чистимо локальне сховище
+
 function onSubmitForm(e) {
   e.preventDefault();
   if (emailInput.value === '' || messagelInput.value === '') {
@@ -26,4 +26,6 @@ function onSubmitForm(e) {
   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   e.target.reset();
   localStorage.removeItem('feedback-form-state');
+  obj.email = '';
+  obj.message = '';
 }
